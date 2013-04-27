f266250140144ec45da2a614e2bd1b49ab573af0
Evan <iiechapman@gmail.com>
2013-04-27 00:47:16 -0400

    Test Commit

---
+++ LD26Test/LD26Test/main.cpp
@@ -0,0 +1,12 @@
+#include <SFML/System.hpp>
+#include "ResourcePath.hpp"
+#include <iostream>
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+    
+    cout << "This is a test"<< endl;
+
+	return EXIT_SUCCESS;
+}
